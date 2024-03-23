6422135c7b6dd21012635bb6f0a65b6017313af9
Alfredebi9 <alfredsalvadorfav@gmail.com>
2024-03-23 01:34:50 +0100

    addedd

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
+window.location.href = "signin.html";
+
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
